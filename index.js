8b02afbc7fc5d91b81b5a28e7a35e10895e0af94
Philipp_Lypniakov <filipprayden@gmail.com>
2020-10-04 23:48:01 +0300

    Complete CLI functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-const process = require("process");
+const fs = require("fs");
+const path = require("path");
 const sade = require("sade");
 const readline = require("readline");
 
@@ -11,12 +12,24 @@ prog
   .option("-s, --shift", "A shift by how many characters to move an alphabet")
   .option("-i, --input", "Provide path to an input file")
   .option("-o, --output", "Change the name of the output file")
-  .option("-a, --action", "Change the name of the output file")
+  .option("-a, --action", "An action, encode/decode")
   .example('-a encode -s 7 -i "./input.txt" -o "./output.txt"')
   .example("--action encode --shift 7 --input plain.txt --output encoded.txt")
   .example("--action decode --shift 7 --input decoded.txt --output plain.txt")
   .action((dest) => {
-    if (!(dest.i && dest.o)) {
+    const { action, shift, input, output } = dest;
+
+    if (!action) {
+      console.error("Action flag is missing!");
+      process.exitCode = 1;
+    }
+
+    if (!shift) {
+      console.error("Shift flag is missing!");
+      process.exitCode = 1;
+    }
+
+    if (!input && !output) {
       const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -27,6 +40,19 @@ prog
         console.log(`> building from to ${JSON.stringify(dest)}`);
       });
     } else {
+      const readStream = input
+        ? fs.createReadStream(path.resolve(__dirname, input))
+        : process.stdin;
+      const writeStream = output
+        ? fs.createWriteStream(path.resolve(__dirname, output), { flags: "a" })
+        : process.stdout;
+
+      readStream.on("error", () => {
+        console.error("File to be read is unavailable or invalid!");
+        process.exitCode = 2;
+      });
+
+      readStream.pipe(writeStream);
     }
   })
   .parse(process.argv);
